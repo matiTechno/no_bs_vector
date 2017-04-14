25a96fddf1bd1cc771e2455ae6456bf844aa534a
mati <mateusz.macieja8@gmail.com>
2017-04-15 00:47:19 +0200

    all functions implemented. to do: exception safety && tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,125 @@
 #include <iostream>
+#include "vector.hpp"
+#include <string>
+#include <chrono>
+#include <vector>
+#include <memory>
 
-using namespace std;
+class My_class
+{
+public:
+    int x = 55;
+    short zz = 1000, xx;
+    std::string string = "hello";
+    bool lol;
+    bool xw = false;
+    double d1, d2, d3, d4;
+};
+
+class Lol
+{
+public:
+    Lol(const std::string& string, int x, bool y):
+        string(string),
+        x(x),
+        y(y)
+    {
+        y = !y;
+    }
+    std::string string;
+    int x;
+    bool y;
+};
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    mtech::vector<My_class> mtech_vector;
+    mtech_vector.reserve(100);
+    std::vector<My_class> stl_vector;
+    stl_vector.reserve(100);
+    {
+        auto time = std::chrono::high_resolution_clock::now();
+
+        for(std::size_t i = 0; i < 1000000; ++i)
+            mtech_vector.push_back(My_class());
+
+        auto new_time = std::chrono::high_resolution_clock::now();
+        std::cout << "mtech time(s): " << std::chrono::duration<float>(new_time - time).count() << std::endl;
+    }
+    {
+        auto time = std::chrono::high_resolution_clock::now();
+
+        for(std::size_t i = 0; i < 1000000; ++i)
+            stl_vector.push_back(My_class());
+
+        auto new_time = std::chrono::high_resolution_clock::now();
+        std::cout << "stl time(s): " << std::chrono::duration<float>(new_time - time).count() << std::endl;
+    }
+
+    auto new_vec1 = mtech_vector;
+    auto new_vec2 = std::move(new_vec1);
+    new_vec2.insert(new_vec2.begin() + 1, My_class());
+
+
+    mtech::vector<int> ivec{10, 20, 30, 50, 60};
+    ivec.insert(ivec.begin() + 1, 666);
+    ivec.emplace(ivec.end() - 1, 69);
+    for(auto& i: ivec)
+        std::cout << i << std::endl;
+
+    // test if all functions compile
+    {
+        mtech::vector<Lol> test;
+        test = mtech::vector<Lol>(1000, Lol("x", 5, true));
+        test = mtech::vector<Lol>{Lol("x", 5, true), Lol("x", 5, true), Lol("x", 5, true), Lol("x", 5, true)};
+        auto new_vec = test;
+        test = new_vec;
+        test = std::move(new_vec);
+        auto it1 = test.begin();
+        it1 = test.end();
+        auto it2 = test.cbegin();
+        it2 = test.cend();
+        test.reserve(1000);
+        test.empty();
+        test.size();
+        test.capacity();
+        auto* x = test.data();
+        (void)x;
+        x = &test.back();
+        const auto* y = test.data();
+        (void)y;
+        y = &test.back();
+        y = &test[0];
+        x = &test[0];
+        y = &test.front();
+        x = &test.front();
+        test.insert(test.begin() + 2, Lol("x", 5, true));
+        test.emplace(test.begin() + 2, "x", 5, true);
+        test.erase(test.begin());
+        test.erase(test.begin(), test.end() - 3);
+        test.push_back(Lol("lol", 10, false));
+        test.emplace_back("xxx", 20, true);
+        test.pop_back();
+        auto b(it1 == it2);
+        (void)b;
+        b = it1 != it2;
+        b = it1 < it2;
+        b = it1 > it2;
+        b = it1 <= it2;
+        b = it1 >= it2;
+        ++it2;
+        --it2;
+        it2 += 5;
+        it2 - 5;
+        it2 + 5;
+        it2 -= 5;
+        it2 - 5;
+        it2 - it1;
+        *it2;
+        auto z = it1->x;
+        ++z;
+        test.clear();
+    }
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
--- src/vector.hpp
+++ src/vector.hpp
@@ -1,12 +1,22 @@
-#pragma once
+﻿#pragma once
 
 #include <cstddef>
 #include <iterator>
 #include <initializer_list>
+#include <new>
+#include "assert.h"
 
 namespace mtech
 {
 
+// to do: case of allocation failure
+// ...... object creation failure
+// testing
+// emplecing and exception safety
+// some definitions are outside class to avoid syntax bugs
+// (I don't care about inlining stuff for now)
+// why scope resolution operator is not needed for iterator as parameter type
+// in vector functions outside class definition
 template<typename T>
 class vector
 {
@@ -14,56 +24,202 @@ public:
     class iterator;
     class const_iterator;
 
-    vector();
-    explicit vector(std::size_t count, const T& value = T());
-    vector(std::initializer_list<T> init);
+    vector():
+        m_capacity(1),
+        m_size(0)
+    {
+        mem = static_cast<T*>(operator new(m_capacity * sizeof(T)));
+    }
+    explicit vector(std::size_t count, const T& value = T()):
+        vector()
+    {
+        reserve(count);
+        for(std::size_t i = 0; i < count; ++i)
+            push_back(value);
+    }
 
-    vector(const vector& rhs);
-    vector(vector&& rhs);
-    vector& operator=(const vector& rhs);
-    vector& operator=(vector&& rhs);
-    ~vector();
+    vector(std::initializer_list<T> init):
+        vector()
+    {
+        reserve(init.size());
+        for(auto& item: init)
+            push_back(std::move(item));
+    }
+
+    vector(const vector& rhs):
+        vector()
+    {
+        reserve(rhs.m_capacity);
+        mem = static_cast<T*>(::operator new(m_capacity * sizeof(T)));
+        for(auto it = rhs.cbegin(); it != rhs.cend(); ++it)
+            push_back(*it);
+    }
+
+    vector(vector&& rhs):
+        mem(rhs.mem),
+        m_capacity(rhs.m_capacity),
+        m_size(rhs.m_size)
+    {
+        rhs.m_size = 0;
+        rhs.mem = nullptr;
+    }
+
+    vector& operator=(const vector& rhs)
+    {
+        if(this == &rhs)
+            return *this;
+
+        if(rhs.m_capacity > m_capacity)
+            reserve(rhs.m_capacity);
+
+        std::size_t i = 0;
+        for(; i < m_size && rhs.m_size; ++i)
+            operator[](i) = rhs[i];
+        for(; i < rhs.m_size; ++i)
+            push_back(rhs[i]);
+        for(; i < m_size; ++i)
+            operator[](i).~T();
+
+        return *this;
+    }
+
+    vector& operator=(vector&& rhs)
+    {
+        if(this == &rhs)
+            return *this;
+
+        this->~vector();
+        mem = rhs.mem;
+        m_capacity = rhs.m_capacity;
+        m_size = rhs.m_size;
+
+        rhs.m_size = 0;
+        rhs.mem = nullptr;
+
+        return *this;
+    }
+
+    ~vector()
+    {
+        for(auto it = begin(); it != end(); ++it)
+            it->~T();
+        ::operator delete(mem);
+    }
 
     // iterators
-    iterator begin();
-    const iterator cbegin() const;
-    iterator end();
-    const iterator cend() const;
+    iterator begin()
+    {return iterator(mem);}
+
+    const_iterator cbegin() const
+    {return const_iterator(mem);}
+
+    iterator end()
+    {return iterator(mem + m_size);}
+
+    const_iterator cend() const
+    {return const_iterator(mem + m_size);}
 
     // capacity
-    void reserve(std::size_t new_cap);
-    void shrink_to_fit();
-    bool empty() const;
-    std::size_t size() const;
-    std::size_t capacity() const;
+    void reserve(std::size_t new_cap)
+    {
+        if(m_capacity >= new_cap)
+            return;
+
+        T* temp_mem = static_cast<T*>(operator new(new_cap * sizeof(T)));
+
+        for(std::size_t i = 0; i < m_size; ++i)
+            new(temp_mem + i) T(std::move(operator[](i)));
+
+        this->~vector();
+        mem = temp_mem;
+        m_capacity = new_cap;
+    }
+
+    bool empty() const
+    {return !m_size;}
+    std::size_t size() const
+    {return m_size;}
+    std::size_t capacity() const
+    {return m_capacity;}
 
     // element access
-    T* data();
-    const T* data() const;
-    T& front();
-    const T& front() const;
-    T& back();
-    const T& back() const;
-    T& at(std::size_t pos);
-    const T& at(std::size_t pos) const;
-    T& operator[](std::size_t pos);
-    const T& operator[](std::size_t pos) const;
+    T* data()
+    {return mem;}
+
+    const T* data() const
+    {return mem;}
+
+    T& front()
+    {return *mem;}
+
+    const T& front() const
+    {return *mem;}
+
+    T& back()
+    {return *(mem + m_size - 1);}
+
+    const T& back() const
+    {return *(mem + m_size - 1);}
+
+    T& operator[](std::size_t pos)
+    {return *(mem + pos);}
+
+    const T& operator[](std::size_t pos) const
+    {return *(mem + pos);}
 
     // modifiers
-    void clear();
-    // insert
-    // emplace
-    // erase
-    // push_back
-    // emplace_back
-    // pop_back
-    // resize
-    // swap ?
+    void clear()
+    {
+        for(auto it = begin(); it != end(); ++it)
+            it->~T();
+        m_size = 0;
+    }
+
+    template<typename U>
+    iterator insert(const_iterator pos, U&& item);
+
+    // decltype(auto) just 4fun
+    template<typename ...Args>
+    decltype(auto) emplace(const_iterator pos, Args&&... params);
+
+    iterator erase(const_iterator pos);
+
+    iterator erase(const_iterator first, const_iterator last);
+
+    template<typename U>
+    void push_back(U&& item)
+    {
+        if_full();
+        new(mem + m_size) T(std::forward<U>(item));
+        ++m_size;
+    }
+
+    template<typename ...Args>
+    void emplace_back(Args&&... params)
+    {
+        if_full();
+        new(mem + m_size) T(std::forward<Args>(params)...);
+        ++m_size;
+    }
+
+    void pop_back()
+    {
+        (mem + m_size - 1)->~T();
+        --m_size;
+    }
 
 private:
     T* mem;
     std::size_t m_capacity;
     std::size_t m_size;
+
+    void if_full()
+    {
+        if(m_size == m_capacity)
+            reserve(m_capacity * 2 + 1);
+    }
+
+    void shift_to_end_by_one(const_iterator last_excluded);
 };
 
 template<typename T>
@@ -72,21 +228,151 @@ class vector<T>::const_iterator
 public:
     typedef std::ptrdiff_t difference_type;
     typedef T value_type;
-    typedef T* pointer;
-    typedef T& reference;
+    typedef const T* pointer;
+    typedef const T& reference;
     typedef std::random_access_iterator_tag iterator_category;
 
-private:
+    const_iterator() = default;
+    const_iterator(T* ptr):
+        ptr(ptr)
+    {}
+
+    bool operator==(const const_iterator& rhs) const
+    {return ptr == rhs.ptr;}
+    bool operator!=(const const_iterator& rhs) const
+    {return ptr != rhs.ptr;}
+    bool operator<(const const_iterator& rhs) const
+    {return ptr < rhs.ptr;}
+    bool operator>(const const_iterator& rhs) const
+    {return ptr > rhs.ptr;}
+    bool operator<=(const const_iterator& rhs) const
+    {return ptr <=rhs.ptr;}
+    bool operator>=(const const_iterator& rhs) const
+    {return ptr >= rhs.ptr;}
+    const_iterator& operator++()
+    {++ptr; return *this;}
+    const_iterator& operator--()
+    {--ptr; return *this;}
+    const_iterator& operator+=(std::size_t count)
+    {ptr += count; return *this;}
+    const_iterator operator+(std::size_t count) const
+    {return ptr + count;}
+    const_iterator& operator-=(std::size_t count)
+    {ptr -= count; return *this;}
+    const_iterator operator-(std::size_t count) const
+    {return ptr - count;}
+    std::ptrdiff_t operator-(const const_iterator& rhs)
+    {return this->ptr - rhs.ptr;}
+    const T& operator*() const
+    {return *ptr;}
+    const T* operator->() const
+    {return ptr;}
+
+protected:
+    T* ptr;
 };
 
 template<typename T>
 class vector<T>::iterator: public const_iterator
 {
 public:
+    typedef T* pointer;
+    typedef T& reference;
 
-private:
+    iterator() = default;
+    iterator(T* ptr):
+        const_iterator(ptr)
+    {}
+    explicit iterator(const_iterator it):
+        const_iterator(it)
+    {}
+
+    iterator& operator++()
+    {++this->ptr;return *this;}
+    iterator& operator--()
+    {--this->ptr;return *this;}
+    iterator& operator+=(std::size_t count)
+    {this->ptr += count; return *this;}
+    iterator operator+(std::size_t count) const
+    {return this->ptr + count;}
+    iterator& operator-=(std::size_t count)
+    {this->ptr -= count; return *this;}
+    iterator operator-(std::size_t count) const
+    {return this->ptr - count;}
+    T& operator*()
+    {return const_cast<T&>(const_iterator::operator*());}
+    T* operator->()
+    {return const_cast<T*>(const_iterator::operator->());}
 };
 
-#include "vector_impl.hpp"
+template<typename T>
+void vector<T>::shift_to_end_by_one(const_iterator last_excluded)
+{
+    new(&*end()) T(std::move(*(end() - 1)));
+    ++m_size;
 
+    for(auto it = end() - 2; it != last_excluded; --it)
+        *it = std::move(*(it - 1));
 }
+
+template<typename T>
+template<typename U>
+typename vector<T>::iterator vector<T>::insert(const_iterator pos, U&& item)
+{
+    if(pos == end())
+    {
+        if_full();
+        push_back(std::forward<U>(item));
+        return end() - 1;
+    }
+    auto diff = pos - begin();
+    if_full();
+    auto new_pos = begin() + diff;
+    shift_to_end_by_one(new_pos);
+    *new_pos = T(std::forward<U>(item));
+    return new_pos;
+}
+
+template<typename T>
+template<typename ...Args>
+decltype(auto) vector<T>::emplace(const_iterator pos, Args&&... params)
+{
+    if(pos == end())
+    {
+        if_full();
+        emplace_back(std::forward<Args>(params)...);
+        return end() - 1;
+    }
+    auto diff = pos - begin();
+    if_full();
+    auto new_pos = begin() + diff;
+    shift_to_end_by_one(new_pos);
+    new_pos->~T();
+    new(&*new_pos) T(std::forward<Args>(params)...);
+    return new_pos;
+}
+
+template<typename T>
+typename vector<T>::iterator vector<T>::erase(const_iterator pos)
+{
+    for(auto it = iterator(pos); it != end() - 1; ++it)
+        *it = std::move(*(it + 1));
+    (end() - 1)->~T();
+    --m_size;
+
+    return iterator(pos);
+}
+
+template<typename T>
+typename vector<T>::iterator vector<T>::erase(const_iterator first, const_iterator last)
+{
+    auto to_del = last - first + 1;
+    for(auto it = iterator(first); it != end() - to_del; ++it)
+        *it = std::move(*(it + to_del));
+    (end() - 1)->~T();
+    m_size -= to_del;
+
+    return iterator(first);
+}
+
+} // mtech
